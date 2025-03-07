5ae60c0aba3689d932f634f2e1a305730487b9a3
oneblueandred <150866240+oneblueandred@users.noreply.github.com>
2025-03-07 01:37:34 -0800

    added unique ids for files downloaded to r function to avoid conflict

--- synthetic_faas_function.R
+++ synthetic_faas_function.R
@@ -1,14 +1,23 @@
 synthetic_faas_function <- function(folder, execution_time, input_files, output_size_in_bytes) {
+
     #Download input files from S3
     for(file in input_files) {
-        faasr_get_file(remote_folder=folder, remote_file=file, local_file=file)
+        tryCatch({
+            faasr_log(paste0("Downloading file: ", file))
+        faasr_get_file(remote_folder=folder, remote_file=file, local_file=tempfile(pattern=file))
+        }, error = function(e) {
+            faasr_log(paste0("ERROR: Failed to download: ", file))
+            stop(e)
+        }
+        )
     }
 
     #Simulate functions execution
     Sys.sleep(execution_time)
 
     #Open a binary connection for output file
-    con <- file("output.bin", "wb")
+    output_file <- tempfile(pattern="output", fileext=".bin")
+    con <- file(output_file, "wb")
     on.exit(close(con))
 
     # Moves file pointer to output_size_in_bytes - 1 and writes a byte to file
@@ -16,9 +25,9 @@ synthetic_faas_function <- function(folder, execution_time, input_files, output_
     writeBin(as.raw(0), con)
 
     #Store output file in S3
-    faasr_put_file(local_file="output.bin", remote_folder=folder, remote_file="output.bin")
+    faasr_put_file(local_file=output_file, remote_folder=folder, remote_file=output_file)
 
     #Log function
-    log_msg <- paste0('Function synthetic_faas_function finished; output written to ', folder, '/', "output.bin", ' in default S3 bucket')
+    log_msg <- paste0('Function synthetic_faas_function finished; output written to ', folder, '/', output_file, ' in default S3 bucket')
     faasr_log(log_msg)
 }
